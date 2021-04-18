23493d14db3d88faefee50a4866213b60dc917bd
himbazayves <45237897+himbazayves@users.noreply.github.com>
2021-04-18 09:02:25 +0200

    setting up static files

--- Essentials/urls.py
+++ Essentials/urls.py
@@ -1,7 +1,18 @@
+from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.urls import path
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from core.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', homepage, name='homepage'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
